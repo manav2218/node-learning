04e6702746266fe56b8c718f276397bc1c5b44b8
Manvendra Singh <manav2218@gmail.com>
2025-06-23 18:39:18 +0530

    main

---
+++ todo/todo.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const filepath = "./tasks.json";
+
+const loadTasks = () => {
+    try {
+        const dataBuffer = fs.readFileSync(filepath);
+        const dataJSON =  dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch (error) {
+        return [];
+    }
+}
+
+const saveTask = (tasks) => {
+    const dataJSON = JSON.stringify(tasks);
+    fs.writeFileSync(filepath, dataJSON);
+}
+
+const addTask = (task) => {
+    const tasks = loadTasks();
+    tasks.push({task});//can pass task as a normal string as well instead of object
+    saveTask(tasks);
+    console.log("Task added", task);
+}
+
+const listTasks = () => {
+    const tasks = loadTasks();
+    tasks.forEach((task, index) => {
+        console.log(`${index+1} -> ${task.task}`)
+    })
+}
+
+
+const command = process.argv[2]
+const argument = process.argv[3]
+
+
+
+if(command === "add"){
+    addTask(argument);
+}
+else if(command === "list"){
+    listTasks();
+}
+else if(command === "remove"){
+    removeTask(parseInt(argument));
+}
+else{
+    console.log("Command not found");
+}
+
